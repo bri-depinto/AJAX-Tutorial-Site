032393aed4e08b453149493869bd83d98bfd73dd
bri.depinto <briannadepinto@gmail.com>
2024-10-21 13:44:20 -0400

    stopped at 20 mins

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+var btn = document.getElementById("btn");
 
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.onload = function(){
+    var ourData = JSON.parse(ourRequest.responseText);
+   renderHTML(ourData);
+    };
+    ourRequest.send();
+});
+
+function renderHTML(data) {
+
+}
